5db08d17d6a1ceeba343769ef38f3d9146e222aa
Ampara <ampra14131412@gmail.com>
2026-08-05 09:36:37 +0700

    beta1

--- lib/main.dart
+++ lib/main.dart
@@ -31,21 +31,6 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     super.dispose();
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-    if (state == AppLifecycleState.resumed) {
-      // เมื่อแอปกลับมาทำงาน ให้ไปที่ SplashScreen
-      // การทำเช่นนี้จะล้างหน้าจอเก่าทั้งหมดออก
-      _navigatorKey.currentState?.pushAndRemoveUntil(
-        PageRouteBuilder(
-          pageBuilder: (_, _, _) => const SplashScreen(),
-          transitionDuration: const Duration(seconds: 0),
-        ),
-        (route) => false,
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
